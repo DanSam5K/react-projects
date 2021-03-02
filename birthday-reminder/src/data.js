7af3d184c4a2cdc3882a01333cb25e670ca5607e
Daniel Samuel <dansam5000@gmail.com>
2021-03-02 13:43:55 +0100

    birthday reminder app useState react technology

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    id: 1,
+    name: 'Daniel Samuel',
+    age: 16,
+    image: 'https://avatars.githubusercontent.com/u/50230297?s=60&v=4',
+  },
+  {
+    id: 2,
+    name: 'Suzan Luka',
+    age: 10,
+    image:
+      'https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-9/81410015_486244772275926_636673191611203584_o.jpg?_nc_cat=102&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeH8qn4Tf6a7JjJvJ9g6ktQIiioL-IU-hbSKKgv4hT6FtAf9HmOyp5REoV5H64V2G6Cp6TyJaQI6GJ4t0NHYa80z&_nc_ohc=BZkJhfOgmR0AX-2PE9r&_nc_ht=scontent.flos3-1.fna&oh=02d06eeb8aa3eee81c287c84ef551a58&oe=60525BF2',
+  },
+  {
+    id: 3,
+    name: 'Isaac Emmanuel',
+    age: 11,
+    image:
+      'https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/127504324_10218240012020704_6359420166985813336_o.jpg?_nc_cat=111&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeHLnQSEiUTDAXduavVSVKoZ0skdVigRhj7SyR1WKBGGPgxFdwaagchsPOtNxVuPIXgY_DV2yNz6QzbU_s7rtciV&_nc_ohc=b-L24ycKvpAAX-uatvD&_nc_ht=scontent.flos3-2.fna&oh=bbca9fb379e2594564077ae2900cdaa3&oe=60508F70',
+  },
+  {
+    id: 4,
+    name: 'Zizoh Anto',
+    age: 20,
+    image:
+      'https://scontent.flos3-2.fna.fbcdn.net/v/t1.0-9/82106525_10221525319471621_3896887122593841152_o.jpg?_nc_cat=110&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeE6oj7BMYxEQFPHAzuOU6KXcUjt8hErweNxSO3yESvB43VEOMxE6q2rtVSihZkh3Gw97p8SYD7eOK34PDOnRnIs&_nc_ohc=_0gqq81BifwAX8djmNF&_nc_ht=scontent.flos3-2.fna&oh=597fa8b2d448dade9c306e65c6c06abe&oe=6052F87E',
+  },
+  {
+    id: 5,
+    name: 'Mercy Ankuma',
+    age: 13,
+    image:
+      'https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-9/125091433_2799655126985139_2682991091825055045_o.jpg?_nc_cat=106&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeE3md5IL-PRrgokn4tVLAMygetD-9UQdw-B60P71RB3D1_Uv7vu7UJ8ozZrsK8xo4rIFubmLt7glGlV8b84kefD&_nc_ohc=-04_GjE_w-MAX8ufYUH&_nc_ht=scontent.flos3-1.fna&oh=50c5901da0bb314594ec3dac10e006e9&oe=6052A694',
+  },
+  {
+    id: 6,
+    name: 'David Samuel',
+    age: 8,
+    image:
+      'https://scontent.flos3-1.fna.fbcdn.net/v/t1.0-9/147942602_553326415626697_9139009826853514888_o.jpg?_nc_cat=100&ccb=3&_nc_sid=09cbfe&_nc_eui2=AeHIICzeEpd4RY4cNempM-oJP_eOmnWZT1A_946adZlPUKLvXBNY05WtbugzCXF5TbSXfjQqRsq9rDoockAd8EPN&_nc_ohc=Vy1ICaeX4t4AX_bj1V7&_nc_ht=scontent.flos3-1.fna&oh=23d41d6c64ee2c8fad3c18b865644a1b&oe=60506B1E',
+  },
+
+  {
+    id: 7,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
